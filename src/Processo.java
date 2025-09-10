cfe23829648d1493fd0ab5e9b5b708bd64f53d85
gabbms <gabriel.msousz@gmail.com>
2025-09-09 21:53:15 -0300

    feat: adicionando novas funcionalidades

--- src/Processo.java
+++ src/Processo.java
@@ -1,16 +1,18 @@
 public class Processo {
     private int id;
     private String nome;
-    private int prioridade;
+    private int prioridade; // 1 - alta  2 - média  3 - baixa
     private int ciclosNecessarios;
-    private String recursoNecessario;
+    private String recursoNecessario; // DISCO ou NULL
+    private boolean foiBloqueado;
 
     public Processo(int id, String nome, int prioridade, int ciclosNecessarios, String recursoNecessario) {
         this.id = id;
         this.nome = nome;
         this.prioridade = prioridade;
         this.ciclosNecessarios = ciclosNecessarios;
-        this.recursoNecessario = recursoNecessario;
+        this.recursoNecessario = (recursoNecessario == null || recursoNecessario.equalsIgnoreCase("null") || recursoNecessario.trim().isEmpty()) ? null : recursoNecessario;
+        this.foiBloqueado = false;
     }
 
     public int getId() {return id;}
@@ -23,10 +25,19 @@ public class Processo {
 
     public String getRecursoNecessario() {return recursoNecessario;}
 
-    public void setCiclosNecessarios(int ciclos) {
-            this.ciclosNecessarios = ciclos;
+    public boolean foiBloqueado(){
+        return foiBloqueado;
     }
 
+
+    public void decrementarCiclos(){
+        if(this.ciclosNecessarios > 0){
+            this.ciclosNecessarios--;
+        }
+    }
+
+
+    @Override
     public String toString(){
         return String.format("[ID: %d, Nome: %s, Prioridade: %d, Ciclos: %d]", id, nome, prioridade, ciclosNecessarios);
     }
